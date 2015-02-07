176e7a8fc3c52a6c9a849111c20d0d6d4f4be0ed
Ryan Hudson <ryan@hellohudson.com.au>
2015-02-07 22:28:31 +1000

    fix content page

--- page.php
+++ page.php
@@ -6,6 +6,13 @@ Template Name: Standard Page
 
 <main>
 
-	<section class="title">
-	
+	<section class="content">
+		<div class="container">
+			<?php while (have_posts()) : the_post(); ?>
+			<h1>
+			  <?php echo roots_title(); ?>
+			</h1>
+			<?php the_content(); ?>
+			<?php endwhile; ?>
+		</div>
 	</section>
